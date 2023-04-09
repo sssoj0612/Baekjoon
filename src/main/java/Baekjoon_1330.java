09b0e76f35ac444d3afe27460c6195022321f1d3
Kimsojeong <127270665+sssoj0612@users.noreply.github.com>
2023-04-09 16:18:38 +0900

    Create Baekjoon_1330.java
    두 정수 A, B를 입력 받고 비교하는 기호 출력

---
+++ src/main/java/Baekjoon_1330.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a,b;
+
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+
+        if (a>b){
+            System.out.println('>');
+        }
+        else if (a<b){
+            System.out.println('<');
+        }
+        else
+            System.out.println("==");
+    }
+}
