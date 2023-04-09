777706e517d2e8882764d2c7f496b5f0063338c8
Kimsojeong <127270665+sssoj0612@users.noreply.github.com>
2023-04-09 16:08:31 +0900

    2023.04.09
    문장 입력 받은 뒤 "??!"를 붙여 출력

---
+++ src/main/java/Baekjoon_10926.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Baekjoon_10926 {
+    public static void main(String[] args) {
+
+        String id;
+
+        Scanner s = new Scanner(System.in);
+        id = s.nextLine();
+
+        System.out.println(id + "??!");
+
+    }
+}
