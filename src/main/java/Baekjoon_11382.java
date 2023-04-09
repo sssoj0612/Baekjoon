350f65681fd3b43eca123f03232a1e854046ab11
Kimsojeong <127270665+sssoj0612@users.noreply.github.com>
2023-04-09 16:16:22 +0900

    2023.04.09
     A, B, C (1 ≤ A, B, C ≤ 1012) 정수를 입력받아 합을 출력

---
+++ src/main/java/Baekjoon_11382.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        long a,b,c;
+
+        Scanner s = new Scanner(System.in);
+        a = s.nextLong();
+        b = s.nextLong();
+        c = s.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
