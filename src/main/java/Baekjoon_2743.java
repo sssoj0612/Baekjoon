f16f308aa9f085b0d1eb0bf391389083c52cd2b7
chlwnstkd <2320110213@office.kopo.ac.kr>
2023-05-24 08:50:36 +0900

    2023.05.24 문자열을 입력받고 몇 글자인지 출력

---
+++ src/main/java/Baekjoon_2743.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Baekjoon_2743 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int len = str.length();
+
+        System.out.print(len);
+    }
+}
