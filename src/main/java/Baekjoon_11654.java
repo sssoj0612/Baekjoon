1c2710761b686ec3e933b103908a6b98476c18a6
Kimsojeong <sssoj0612@gmail.com>
2023-05-26 20:22:24 +0900

    2023.05.26 알파벳 소문자,대문자,숫자 중 하나가 주어졌을 때 아스키 코드 출력

---
+++ src/main/java/Baekjoon_11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Baekjoon_11654 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        char c = s.next().charAt(0);
+        int ascii = (int)c;
+
+        System.out.print(ascii);
+    }
+}
