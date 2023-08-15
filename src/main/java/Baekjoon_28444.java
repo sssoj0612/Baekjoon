4fc498a882f5ba6ee237c1cf6e3e31ce36765f63
Kimsojeong <sssoj0612@gmail.com>
2023-08-15 16:05:08 +0900

    2023.08.15 5개 정수 사이에 공백을 두고 입력 받은 뒤 수식에 따라 출력

---
+++ src/main/java/Baekjoon_28444.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon_28444 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        StringTokenizer nums = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(nums.nextToken());
+        int i = Integer.parseInt(nums.nextToken());
+        int a = Integer.parseInt(nums.nextToken());
+        int r = Integer.parseInt(nums.nextToken());
+        int c = Integer.parseInt(nums.nextToken());
+
+        bw.write( Integer.toString((h*i) - (a*r*c)) );
+        bw.newLine();
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
