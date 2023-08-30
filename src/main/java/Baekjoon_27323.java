3bd469d052893f5c5db472d4c2121f7ccdd2d2b0
chlwnstkd <2320110213@office.kopo.ac.kr>
2023-08-30 11:26:30 +0900

    2023.08.30 직사각형 가로, 세로 입력받고 넓이 구하기

---
+++ src/main/java/Baekjoon_27323.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Baekjoon_27323 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        int result = a*b;
+
+        System.out.print(result);
+    }
+}
